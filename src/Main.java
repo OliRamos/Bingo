c4ade1bf43f17c05876d89a5163b5995f355fe64
OliRamos <oli.ramos@ymail.com>
2023-11-14 09:48:43 -0300

    Inicio projeto

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------- ");
+        System.out.println(" B I N G O  D O S  'J U N I N'  5 0 + ");
+        System.out.println("---------------------------------------- \n");
+
+        System.out.println("Entre com os nomes dos participantes separadas por vírgula ','.");
+        System.out.println("Exemplo: Fulano, Cicrana, Beltrana");
+
+
+
+
+    }
+}
