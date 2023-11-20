e8f4243189e08da7159bdcc658428def179ea6b0
OliRamos <oli.ramos@ymail.com>
2023-11-20 13:05:44 -0300

    Geracao de cartelas de acordo com o numero de jogadores

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -10,33 +9,55 @@ public class Main {
         System.out.println(" B I N G O  D O S  'J U N I N'  5 0 + ");
         System.out.println("---------------------------------------- \n");
         System.out.println("Entre com os nomes dos participantes separadas por vírgula ','.");
-        System.out.println("Exemplo: Fulano, Cicrana, Beltrana:");
-        System.out.print("Digite os nomes aqui: -> ");
+        System.out.println("Exemplo: Fulano-Cicrana-Beltrana");
+        System.out.print("Digite os nomes aqui: >> ");
         String entradaNomes = sc.nextLine();
 
-        String[] nomesDosJogadores = entradaNomes.split(", ");
-        String[][] cartelasJogadores = new String[nomesDosJogadores.length][6];
+        String[] nomesDosJogadores = entradaNomes.split("-");
         int qtdNumerosCartela = 5;
 
-        int[] cartela = sorteiaNumerosCartela(qtdNumerosCartela);
+        String[][] cartelasJogadores = new String[nomesDosJogadores.length][qtdNumerosCartela];
+        for (int i = 0; i < nomesDosJogadores.length; i++) {
+            cartelasJogadores[i][0] = nomesDosJogadores[i];
+            int[] cartela = gerarNumerosCartela(qtdNumerosCartela);
+            for(int j = 0; j < qtdNumerosCartela; j++){
+                   cartelasJogadores[i][j] = Integer.toString(cartela[j]);
+            }
+        }
+        for (String[] cartela : cartelasJogadores){
+            System.out.println(Arrays.toString(cartela));
+        }
+
+
+        int[] cartela = gerarNumerosCartela(qtdNumerosCartela);
         System.out.println("numeros sorteados = " + Arrays.toString(cartela));
 
-
     }
 
-    public static int[] sorteiaNumerosCartela(int qtdNumerosCartela) {
-        int[] numerosSorteados = new int[qtdNumerosCartela];
-        boolean[] verificaRepitidos = new boolean[61];
 
-        for (int i = 0; i < qtdNumerosCartela; i++) {
-            do {
-                numerosSorteados[i] = (int) (Math.random() * 60) + 1;
+    public static int[] gerarNumerosCartela(int qtdNumerosCartela) {
+        int[] numerosGerados = new int[qtdNumerosCartela];
+        for(int i = 0; i < qtdNumerosCartela; i++){
+            while (true){
+                int numeroTeste = (int) (Math.random() * 60 + 1);
+                if (verificarRepitidos(numeroTeste,numerosGerados)){
+                    numerosGerados[i] = numeroTeste;
+                    break;
+                }
             }
-            while (verificaRepitidos[numerosSorteados[i]]);
-
         }
-        return numerosSorteados;
+        return numerosGerados;
     }
+
+    public static boolean verificarRepitidos(int numeroTeste, int[] numerosGerados) {
+        for (int i = 0; i < numerosGerados.length; i++) {
+            if (numerosGerados[i] == numeroTeste) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
 
